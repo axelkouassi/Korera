16b5b6359aa89ebf13b433387fbbf0a126a5b0ab
Kouassi Axel Geoffroy KOUASSI <ak659@njit.edu>
2021-08-16 06:59:05 -0400

    Web token is successfully generated when user logs in.

--- src/main/java/com/itlize/korera/controller/UserController.java
+++ src/main/java/com/itlize/korera/controller/UserController.java
@@ -12,7 +12,6 @@ import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
