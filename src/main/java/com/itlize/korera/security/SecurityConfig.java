3a2f880d44214139d06c8b377d4acc314fa8000c
Kouassi Axel Geoffroy KOUASSI <ak659@njit.edu>
2021-08-16 05:14:03 -0400

    Creation of SecurityConfig class to handle authentication and authorization of the application.

---
+++ src/main/java/com/itlize/korera/security/SecurityConfig.java
@@ -0,0 +1,67 @@
+package com.itlize.korera.security;
+
+import com.itlize.korera.filter.JwtRequestFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    UserDetailsService userDetailsService;
+    @Autowired
+    private JwtRequestFilter jwtRequestFilter;
+
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder (){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.csrf().disable()
+                //Cross-origin-resource-sharing
+                .cors().and()
+                .authorizeRequests()
+                .antMatchers("/user/login").permitAll()// Allow  access without logging in
+                .antMatchers("/user/register").permitAll()
+                .antMatchers("/user/Admin/**").hasRole("ADMIN")
+                .anyRequest().fullyAuthenticated()// others need to be accessed after authentication
+                .and()
+                .logout().permitAll()
+                // redirect to the login page
+                .logoutRequestMatcher(new AntPathRequestMatcher("user/logout","POST"))
+                .and().formLogin().loginPage("/user/authenticate").and()
+                .exceptionHandling().and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        httpSecurity.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+    }
+}
