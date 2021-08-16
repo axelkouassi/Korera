914b4058f42b594760e57f51a520568e557a6633
Kouassi Axel Geoffroy KOUASSI <ak659@njit.edu>
2021-08-16 06:29:50 -0400

    Creation of ProjectResourceRepository class.

---
+++ src/main/java/com/itlize/korera/repository/ProjectResourceRepository.java
@@ -0,0 +1,7 @@
+package com.itlize.korera.repository;
+
+import com.itlize.korera.model.ProjectResource;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProjectResourceRepository extends JpaRepository<ProjectResource,Integer> {
+}
