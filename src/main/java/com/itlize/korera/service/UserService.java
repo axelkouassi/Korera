47225087ad30c091e6cdb0ac8405539f38f73fb5
Kouassi Axel Geoffroy KOUASSI <ak659@njit.edu>
2021-08-16 06:31:28 -0400

    Creation of UserService.

---
+++ src/main/java/com/itlize/korera/service/UserService.java
@@ -0,0 +1,15 @@
+package com.itlize.korera.service;
+
+import com.itlize.korera.model.User;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface UserService {
+    User saveUser(User user);
+    User findByUsername(String username);
+    List<User> getUsers();
+    void deleteByUsername(String username);
+    void deleteUsers();
+
+}
