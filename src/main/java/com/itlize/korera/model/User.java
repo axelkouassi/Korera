4cbf565441d916d66342508e708e89abd578629d
Kouassi Axel Geoffroy KOUASSI <ak659@njit.edu>
2021-08-16 03:42:04 -0400

    Creation of User entity/model class.

---
+++ src/main/java/com/itlize/korera/model/User.java
@@ -0,0 +1,140 @@
+package com.itlize.korera.model;
+
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import javax.persistence.*;
+import javax.persistence.Column;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Table(name="user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name="user_id", nullable = false, unique = true, updatable = false)
+    private Integer userId;
+
+    @Column(name="username", nullable = false, unique = true)
+    private String username;
+
+    @Column(name="email", nullable = false, unique = true)
+    private String email;
+
+    @Column(name="password", nullable = false)
+    private String password;
+
+    @Column(name="first_name", nullable = false)
+    private String firstName;
+
+    @Column(name="last_name", nullable = false)
+    private String lastName;
+
+    @Column(name="phone")
+    private Integer phone;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name="role")
+    private Role role = Role.USER;
+
+    @CreatedDate
+    @Column(name="time_created")
+    private LocalDateTime timeCreated;
+
+    @LastModifiedDate
+    @Column(name="time_updated")
+    private LocalDateTime timeUpdated;
+
+    @OneToMany(fetch= FetchType.LAZY, mappedBy="user", cascade= CascadeType.ALL)
+    private List<Project> projects;
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Integer getPhone() {
+        return phone;
+    }
+
+    public void setPhone(Integer phone) {
+        this.phone = phone;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public LocalDateTime getTimeCreated() {
+        return timeCreated;
+    }
+
+    public void setTimeCreated(LocalDateTime timeCreated) {
+        this.timeCreated = timeCreated;
+    }
+
+    public LocalDateTime getTimeUpdated() {
+        return timeUpdated;
+    }
+
+    public void setTimeUpdated(LocalDateTime timeUpdated) {
+        this.timeUpdated = timeUpdated;
+    }
+
+    public List<Project> getProjects() {
+        return projects;
+    }
+
+    public void setProjects(List<Project> projects) {
+        this.projects = projects;
+    }
+}
