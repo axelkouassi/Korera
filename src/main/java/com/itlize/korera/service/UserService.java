3010313fec7984ae931d7727dd6ad918ddc8d4b6
Kouassi Axel Geoffroy KOUASSI <ak659@njit.edu>
2021-08-17 16:33:59 -0400

    Creation of abstract methods in interface UserService to update username, password, email, first name, last name, phone and role.

--- src/main/java/com/itlize/korera/service/UserService.java
+++ src/main/java/com/itlize/korera/service/UserService.java
@@ -1,5 +1,6 @@
 package com.itlize.korera.service;
 
+import com.itlize.korera.model.Role;
 import com.itlize.korera.model.User;
 
 import java.util.List;
@@ -12,6 +13,13 @@ public interface UserService {
     User findByUsername(String username);
     User findByUserId(Integer userId);
     List<User> getUsers();
+    User updateUsername(User user, String username);
+    User updatePassword(User user, String password);
+    User updateEmail(User user, String email);
+    User updateFirstName(User user, String firstName);
+    User updateLastName(User user, String lastName);
+    User updatePhone(User user, Integer phone);
+    User updateRole(User user, Role role);
     void deleteByUsername(String username);
     void deleteByUserId(Integer userId);
     void deleteUsers();
