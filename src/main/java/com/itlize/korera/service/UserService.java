5f46152ea51653144c962d54177777b774bd4d94
Kouassi Axel Geoffroy KOUASSI <ak659@njit.edu>
2021-08-17 12:44:57 -0400

    Creation of abstract method boolean userIdExists(Integer userId) within the interface UserService.

--- src/main/java/com/itlize/korera/service/UserService.java
+++ src/main/java/com/itlize/korera/service/UserService.java
@@ -7,6 +7,7 @@ import java.util.Optional;
 
 public interface UserService {
     boolean usernameExists(String username);
+    boolean userIdExists(Integer userId);
     User saveUser(User user);
     User findByUsername(String username);
     User findByUserId(Integer userId);
