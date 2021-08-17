5c54e37a0a4059efe0a0534b9781020e8d599c78
Kouassi Axel Geoffroy KOUASSI <ak659@njit.edu>
2021-08-17 16:42:48 -0400

    Implementation of all the abstract methods defined in interface UserService to update username, password, email, first name, last name, phone and role, within the UserServiceImpl class.

--- src/main/java/com/itlize/korera/service/UserServiceImpl.java
+++ src/main/java/com/itlize/korera/service/UserServiceImpl.java
@@ -1,5 +1,6 @@
 package com.itlize.korera.service;
 
+import com.itlize.korera.model.Role;
 import com.itlize.korera.model.User;
 import com.itlize.korera.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -79,6 +80,76 @@ public class UserServiceImpl implements UserService{
     public List<User> getUsers() {
         log.info("Fetching all users.");
         return userRepository.findAll();
+    }
+
+    //Update username
+    @Override
+    public User updateUsername(User user, String username) {
+        log.info("Updating username.");
+        user.setUsername(username);
+        user.setTimeUpdated(LocalDateTime.now());
+        log.info("Saving new updated username information to the database.");
+        return userRepository.save(user);
+    }
+
+    //Update password
+    @Override
+    public User updatePassword(User user, String password) {
+        log.info("Updating password.");
+        user.setPassword(password);
+        user.setTimeUpdated(LocalDateTime.now());
+        log.info("Saving new updated password information to the database.");
+        return userRepository.save(user);
+    }
+
+    //Update email
+    @Override
+    public User updateEmail(User user, String email) {
+        log.info("Updating email.");
+        user.setEmail(email);
+        user.setTimeUpdated(LocalDateTime.now());
+        log.info("Saving new updated email information to the database.");
+        return userRepository.save(user);
+    }
+
+    //Update first name
+    @Override
+    public User updateFirstName(User user, String firstName) {
+        log.info("Updating first name.");
+        user.setFirstName(firstName);
+        user.setTimeUpdated(LocalDateTime.now());
+        log.info("Saving new updated first name information to the database.");
+        return userRepository.save(user);
+    }
+
+    //Update last name
+    @Override
+    public User updateLastName(User user, String lastName) {
+        log.info("Updating first name.");
+        user.setLastName(lastName);
+        user.setTimeUpdated(LocalDateTime.now());
+        log.info("Saving new updated last name information to the database.");
+        return userRepository.save(user);
+    }
+
+    //Update phone
+    @Override
+    public User updatePhone(User user, Integer phone) {
+        log.info("Updating first name.");
+        user.setPhone(phone);
+        user.setTimeUpdated(LocalDateTime.now());
+        log.info("Saving new updated last name information to the database.");
+        return userRepository.save(user);
+    }
+
+    //Update phone
+    @Override
+    public User updateRole(User user, Role role) {
+        log.info("Updating first name.");
+        user.setRole(role);
+        user.setTimeUpdated(LocalDateTime.now());
+        log.info("Saving new updated role information to the database.");
+        return userRepository.save(user);
     }
 
     //Delete user by username
