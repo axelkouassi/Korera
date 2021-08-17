128fe0fda5ca89c2e11734e0021a057de6f28d01
Kouassi Axel Geoffroy KOUASSI <ak659@njit.edu>
2021-08-17 13:58:00 -0400

    Creation of ProjectRepositoryTest class to test ProjectRepository.

---
+++ src/test/java/com/itlize/korera/repository/ProjectRepositoryTest.java
@@ -0,0 +1,94 @@
+package com.itlize.korera.repository;
+
+import com.itlize.korera.model.Project;
+import com.itlize.korera.model.User;
+import org.junit.Assert;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+class ProjectRepositoryTest {
+
+    @Autowired
+    UserRepository userRepository;
+    @Autowired
+    ProjectRepository projectRepository;
+
+    //Create Project
+    @Test
+    public void createProjectTest(){
+
+        //Existing user
+        Integer userId = 1;
+        User user;
+        user = userRepository.getById(userId);
+
+        //Create new project
+        Project project = new Project();
+        project.setProjectName("Project 1");
+        project.setUser(user);
+        project.setTimeCreated(LocalDateTime.now());
+        project.setTimeUpdated(LocalDateTime.now());
+
+        Project projectTest = projectRepository.save(project);
+
+        boolean doesProjectExists = projectRepository.existsById(projectTest.getProjectId());
+
+        assertNotNull(projectTest);
+        //assertTrue(doesProjectExists);
+        //assertEquals(project, projectTest);
+    }
+
+    //Read Project
+    @Test
+    public void readProjectByIdTest(){
+        Integer projectId = 1;
+
+        boolean IsProjectExists = projectRepository.existsById(projectId);
+
+        assertTrue(IsProjectExists);
+    }
+
+    //Update project
+    @Test
+    public void updateProjectByIdTest(){
+        Integer projectId= 1;
+        Project expected = projectRepository.getById(projectId);
+        expected.setProjectName("Project1");
+        expected.setTimeUpdated(LocalDateTime.now());
+
+        Project actual = projectRepository.save(expected);
+
+        assertEquals("Project1", actual.getProjectName());
+    }
+
+    //Delete Project
+    @Test
+    public void deleteProjectByIdTest(){
+
+        Integer projectId= 1;
+
+        projectRepository.deleteById(projectId);
+
+        boolean actual = projectRepository.existsById(projectId);
+
+        assertFalse(actual);
+    }
+
+    @Test
+    void findByProjectName() {
+    }
+
+    @Test
+    void deleteByProjectName() {
+    }
+}
