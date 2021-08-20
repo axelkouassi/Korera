a234e7c4516dc4ffbfdd6a810843503c82eb0c26
Kouassi Axel Geoffroy KOUASSI <ak659@njit.edu>
2021-08-20 12:52:22 -0400

    Implementation of boolean resourceNameExists(String name), boolean resourceCodeExists(String code), void deleteByName(String name) and Resource saveResource(Resource resource).

--- src/main/java/com/itlize/korera/service/serviceimpl/ResourceServiceImpl.java
+++ src/main/java/com/itlize/korera/service/serviceimpl/ResourceServiceImpl.java
@@ -2,19 +2,33 @@ package com.itlize.korera.service.serviceimpl;
 
 import com.itlize.korera.model.Project;
 import com.itlize.korera.model.Resource;
+import com.itlize.korera.repository.ResourceRepository;
+import com.itlize.korera.repository.UserRepository;
 import com.itlize.korera.service.ResourceService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
+import java.time.LocalDateTime;
 import java.util.List;
 
+@Service
+@RequiredArgsConstructor
+@Transactional
+@Slf4j // logs
 public class ResourceServiceImpl implements ResourceService {
+    @Autowired
+    private final ResourceRepository resourceRepository;
     @Override
     public boolean resourceNameExists(String name) {
-        return false;
+        return resourceRepository.findByResourceName(name).isPresent();
     }
 
     @Override
     public boolean resourceCodeExists(String code) {
-        return false;
+        return resourceRepository.findByResourceCode(code).isPresent();
     }
 
     @Override
@@ -24,7 +38,9 @@ public class ResourceServiceImpl implements ResourceService {
 
     @Override
     public Resource saveResource(Resource resource) {
-        return null;
+        resource.setTimeCreated(LocalDateTime.now());
+        resource.setTimeUpdated(LocalDateTime.now());
+        return resourceRepository.save(resource);
     }
 
     @Override
@@ -64,7 +80,7 @@ public class ResourceServiceImpl implements ResourceService {
 
     @Override
     public void deleteByName(String name) {
-
+        resourceRepository.deleteByResourceName(name);
     }
 
     @Override
