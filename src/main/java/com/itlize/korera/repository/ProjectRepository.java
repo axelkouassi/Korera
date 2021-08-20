0ac89fbf421ba3f30b2716443b9b3aad52523a73
Kouassi Axel Geoffroy KOUASSI <ak659@njit.edu>
2021-08-20 03:52:39 -0400

    Creation of custom method List<Project> findAllByUser_Username(String username) inside ProjectRepository interface.

--- src/main/java/com/itlize/korera/repository/ProjectRepository.java
+++ src/main/java/com/itlize/korera/repository/ProjectRepository.java
@@ -5,11 +5,13 @@ import com.itlize.korera.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface ProjectRepository extends JpaRepository<Project,Integer> {
     boolean existsByProjectName(String name);
+    List<Project> findAllByUser_Username(String username);
     Optional<Project> findByProjectName(String projectName);
     void deleteByProjectName(String projectName);
 }
