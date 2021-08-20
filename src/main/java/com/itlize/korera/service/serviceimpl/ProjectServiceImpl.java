de8f2edae1b1acd45863f2f1f6a9752227502e90
Kouassi Axel Geoffroy KOUASSI <ak659@njit.edu>
2021-08-19 23:15:00 -0400

    Custom implementation of abstract method saveProject(Project project) from interface ProjectService, within the ProjectServiceImpl class.

--- src/main/java/com/itlize/korera/service/serviceimpl/ProjectServiceImpl.java
+++ src/main/java/com/itlize/korera/service/serviceimpl/ProjectServiceImpl.java
@@ -2,11 +2,28 @@ package com.itlize.korera.service.serviceimpl;
 
 import com.itlize.korera.model.Project;
 import com.itlize.korera.model.User;
+import com.itlize.korera.repository.ProjectRepository;
+import com.itlize.korera.repository.UserRepository;
 import com.itlize.korera.service.ProjectService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
+import java.time.LocalDateTime;
 import java.util.List;
 
+@Service
+@RequiredArgsConstructor
+@Transactional
+@Slf4j // logs
 public class ProjectServiceImpl implements ProjectService {
+
+    @Autowired
+    private final UserRepository userRepository;
+    @Autowired
+    private final ProjectRepository projectRepository;
     @Override
     public boolean projectNameExists(String name) {
         return false;
@@ -19,7 +36,11 @@ public class ProjectServiceImpl implements ProjectService {
 
     @Override
     public Project saveProject(Project project) {
-        return null;
+        log.info("Creating project " + project.getProjectName() + "...");
+        project.setTimeCreated(LocalDateTime.now());
+        project.setTimeUpdated(LocalDateTime.now());
+        log.info("Project " + project.getProjectName() + " has been created and saved to the database.");
+        return projectRepository.save(project);
     }
 
     @Override
