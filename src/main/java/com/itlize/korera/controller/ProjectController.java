32f202fba887700b6fe0f6f3e74531271910a1a6
Kouassi Axel Geoffroy KOUASSI <ak659@njit.edu>
2021-08-19 23:17:32 -0400

    Creation of ProjectController class and controller/api /project/create/{username} to create a project associated with a particular user.

---
+++ src/main/java/com/itlize/korera/controller/ProjectController.java
@@ -0,0 +1,48 @@
+package com.itlize.korera.controller;
+
+import com.itlize.korera.model.Project;
+import com.itlize.korera.model.User;
+import com.itlize.korera.service.ProjectService;
+import com.itlize.korera.service.UserService;
+import com.itlize.korera.util.JwtUtil;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+@RestController
+@RequestMapping("/project")
+@RequiredArgsConstructor
+public class ProjectController {
+
+    @Autowired
+    private UserService userService;
+    @Autowired
+    private ProjectService projectService;
+    @Autowired
+    private AuthenticationManager authenticationManager;
+    @Autowired
+    private JwtUtil jwtTokenUtil;
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    //Controller/api to create a project
+    @PostMapping("/create/{username}")
+    public ResponseEntity<?> createProject(@RequestBody Project project, @PathVariable String username){
+        User user = userService.findByUsername(username);
+        if (!userService.usernameExists(user.getUsername())){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Username \"" + username + "\" does not exists!");
+        }else if (projectService.projectNameExists(project.getProjectName())){
+            throw new ResponseStatusException(HttpStatus.CONFLICT, "Project \"" + project.getProjectName() + "\" already exists!");
+        }else{
+            project.setUser(user);
+            projectService.saveProject(project);
+        }
+        return new ResponseEntity<>("Project " + project.getProjectName() +
+                " has been successfully created.", HttpStatus.CREATED);
+    }
+}
