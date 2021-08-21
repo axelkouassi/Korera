69b67a29604f3e5fbffc7646f457f1501eb551db
Kouassi Axel Geoffroy KOUASSI <ak659@njit.edu>
2021-08-21 04:48:24 -0400

    Definition of abstract methods Optional<Column> findByColumnType(ColumnType type), List<Column> findAllByColumnType(ColumnType type) and boolean existsByColumnType(ColumnType type) within ColumnRepository interface.

--- src/main/java/com/itlize/korera/repository/ColumnRepository.java
+++ src/main/java/com/itlize/korera/repository/ColumnRepository.java
@@ -1,15 +1,20 @@
 package com.itlize.korera.repository;
 
 import com.itlize.korera.model.Column;
+import com.itlize.korera.model.ColumnType;
 import com.itlize.korera.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface ColumnRepository extends JpaRepository<Column,Integer> {
     Optional<Column> findByContent(String content);
+    Optional<Column> findByColumnType(ColumnType type);
+    List<Column> findAllByColumnType(ColumnType type);
     boolean existsByContent(String content);
+    boolean existsByColumnType(ColumnType type);
     void deleteByContent(String content);
 }
