6f217dfc1a9fc51a2abfeb8a819e430c32faa1ae
Kouassi Axel Geoffroy KOUASSI <ak659@njit.edu>
2021-08-23 07:23:07 -0400

    Reorganizing code and removing unused methods' definitions.

--- src/main/java/com/itlize/korera/service/ProjectResourceService.java
+++ src/main/java/com/itlize/korera/service/ProjectResourceService.java
@@ -5,22 +5,18 @@ import com.itlize.korera.model.*;
 import java.util.List;
 
 public interface ProjectResourceService {
-    ProjectResource findById(Integer id);
-    List<ProjectResource> getProjectResources();
-    ProjectResource updateProject(Project project);
-
     boolean projectResourceIdExists(Integer id);
     ProjectResource saveProjectResource(ProjectResource projectResource);
     ProjectResource addResourceToProject(Project project, Resource resource);
-
-    ProjectResource findProjectResourceByProjectName(String projectName);
-    ProjectResource findProjectResourceByResourceName(String resourceName);
-    Project findProjectByProjectResourceId(Integer projectResourceId);
-    Resource findResourceByProjectResourceId(Integer projectResourceId);
-    List<Resource> getResourcesByProject(Project project);
-    List<Project> getProjectsByResource(Resource resource);
-    ProjectResource updateResource(Resource resource);
-    void deleteResourceFromProject(Project project, Resource resource);
+    ProjectResource findById(Integer id);
+    List<ProjectResource> findByProject(String projectName);
+    List<ProjectResource> findByResource(String resourceName);
+    List<ProjectResource> getProjectResources();
+    ProjectResource updateProject(Integer projectResourceId, String projectName);
+    ProjectResource updateResource(Integer projectResourceId, String resourceName);
     void deleteById(Integer id);
+    void deleteByProject(String projectName);
+    void deleteByResource(String resourceName);
     void deleteProjectResources();
+
 }
