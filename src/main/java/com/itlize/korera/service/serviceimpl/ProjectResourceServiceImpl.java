c6efe58797610bd01b22b58ff0d1f5364a89f1db
Kouassi Axel Geoffroy KOUASSI <ak659@njit.edu>
2021-08-23 07:25:19 -0400

    Implementing all abstract methods defined in interface ProjectResourceServiceImpl.

--- src/main/java/com/itlize/korera/service/serviceimpl/ProjectResourceServiceImpl.java
+++ src/main/java/com/itlize/korera/service/serviceimpl/ProjectResourceServiceImpl.java
@@ -3,7 +3,9 @@ package com.itlize.korera.service.serviceimpl;
 import com.itlize.korera.model.Project;
 import com.itlize.korera.model.ProjectResource;
 import com.itlize.korera.model.Resource;
+import com.itlize.korera.repository.ProjectRepository;
 import com.itlize.korera.repository.ProjectResourceRepository;
+import com.itlize.korera.repository.ResourceRepository;
 import com.itlize.korera.repository.UserRepository;
 import com.itlize.korera.service.ProjectResourceService;
 import lombok.RequiredArgsConstructor;
@@ -12,6 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
@@ -22,6 +25,11 @@ public class ProjectResourceServiceImpl implements ProjectResourceService {
 
     @Autowired
     private final ProjectResourceRepository projectResourceRepository;
+    @Autowired
+    private final ResourceRepository resourceRepository;
+    @Autowired
+    private final ProjectRepository projectRepository;
+
     @Override
     public boolean projectResourceIdExists(Integer id) {
         return projectResourceRepository.existsById(id);
@@ -46,23 +54,13 @@ public class ProjectResourceServiceImpl implements ProjectResourceService {
     }
 
     @Override
-    public ProjectResource findProjectResourceByProjectName(String projectName) {
-        return null;
+    public List<ProjectResource> findByProject(String projectName) {
+        return projectResourceRepository.findByProject_ProjectName(projectName);
     }
 
     @Override
-    public ProjectResource findProjectResourceByResourceName(String resourceName) {
+    public List<ProjectResource> findByResource(String resourceName) {
         return projectResourceRepository.findByResource_ResourceName(resourceName);
-    }
-
-    @Override
-    public Project findProjectByProjectResourceId(Integer projectResourceId) {
-        return null;
-    }
-
-    @Override
-    public Resource findResourceByProjectResourceId(Integer projectResourceId) {
-        return null;
     }
 
     @Override
@@ -71,37 +69,50 @@ public class ProjectResourceServiceImpl implements ProjectResourceService {
     }
 
     @Override
-    public List<Resource> getResourcesByProject(Project project) {
-        return null;
+    public ProjectResource updateProject(Integer projectResourceId, String projectName) {
+        ProjectResource projectResource = projectResourceRepository.findById(projectResourceId).orElse(null);
+        Project project = projectRepository.findByProjectName(projectName).orElse(null);
+        if (projectResource != null) {
+            projectResource.setProject(project);
+        }
+        if (projectResource != null) {
+            projectResource.setTimeUpdated(LocalDateTime.now());
+        }
+        return projectResource;
     }
 
     @Override
-    public List<Project> getProjectsByResource(Resource resource) {
-        return null;
-    }
-
-    @Override
-    public ProjectResource updateProject(Project project) {
-        return null;
-    }
-
-    @Override
-    public ProjectResource updateResource(Resource resource) {
-        return null;
-    }
-
-    @Override
-    public void deleteResourceFromProject(Project project, Resource resource) {
-
+    public ProjectResource updateResource(Integer projectResourceId, String resourceName) {
+        ProjectResource projectResource = projectResourceRepository.findById(projectResourceId).orElse(null);
+        Resource resource = resourceRepository.findByResourceName(resourceName).orElse(null);
+        if (projectResource != null) {
+            projectResource.setResource(resource);
+        }
+        if (projectResource != null) {
+            projectResource.setTimeUpdated(LocalDateTime.now());
+        }
+        return projectResource;
     }
 
     @Override
     public void deleteById(Integer id) {
+        projectResourceRepository.findById(id);
 
     }
 
     @Override
-    public void deleteProjectResources() {
+    public void deleteByProject(String projectName) {
+        projectResourceRepository.deleteByProject_ProjectName(projectName);
 
+    }
+
+    @Override
+    public void deleteByResource(String resourceName) {
+        projectResourceRepository.deleteByResource_ResourceName(resourceName);
+    }
+
+    @Override
+    public void deleteProjectResources() {
+        projectResourceRepository.deleteAll();
     }
 }
